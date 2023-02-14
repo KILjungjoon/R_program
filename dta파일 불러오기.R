7fdbf674a9a4ba3cf5f6950d64c57b5d4c7d63e2
Jungjoon Kil <62131328+KILjungjoon@users.noreply.github.com>
2023-02-14 18:58:28 +0800

    Update dta파일 불러오기.R

--- dta파일 불러오기.R
+++ dta파일 불러오기.R
@@ -1,4 +1,10 @@
-setwd("F:\R project\R study\r_grammar")
+setwd("F:/R project/R study/r_grammar")
 
-## 파일 불러오기 : dta파일
+## 패키지는 둘 중 하나를 사용하면 된다. 
+install.packages("haven")
+install.packages("foreign")
+library(haven)
+library(foreign)
+
+## 파일 불러오기 : dta파일(stata 파일)
 dat <- import("Hur_2020_BJPS_KOR.dta")
